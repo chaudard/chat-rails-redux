faad067f64381f927f1d02b056dc56dddaa23aae
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-18 18:19:01 +0200

    seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+general = Channel.create!(name: "general")
+react = Channel.create!(name: "react")
+paris = Channel.create!(name: "paris")
+
+seb = User.create!(email: "seb@gmail.com", password: "123456")
+dan = User.create!(email: "dan@gmail.com", password: "123456")
+
+mes1 = Message.create!(content: "Hello general channel", channel: general, user: dan)
+mes2 = Message.create!(content: "Hello dan on general channel", channel: general, user: seb)
+
+mes3 = Message.create!(content: "Hello react channel", channel: react, user: dan)
+mes4 = Message.create!(content: "Hello dan on react channel", channel: react, user: seb)
+
+mes5 = Message.create!(content: "Hello paris channel", channel: paris, user: dan)
+mes6 = Message.create!(content: "Hello dan on paris channel", channel: paris, user: seb)
